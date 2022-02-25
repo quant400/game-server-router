2c7c85a4674125f606da8639aaa3df0d082cd831
Quant Cooker <85072068+quant400@users.noreply.github.com>
2022-02-25 21:22:21 +0300

    fix only one url bug

--- game.js
+++ game.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = express.Router();
 
-router.get('/',function(req,res){
+app.get('/',function(req,res){
+  res.send("Staging server is running...")
+});
+
+app.get('/chicken-run',function(req,res){
   res.sendFile(path.join(__dirname+'/builds/minigame-chicken-run/WebGL Builds/index.html'));
 });
 
-router.get('/metaverse',function(req,res){
+app.get('/metaverse',function(req,res){
   res.sendFile(path.join(__dirname+'/builds/game-builds/MetaverseBuild/index.html'));
 });
 
-app.use(express.static('builds/game-builds/MetaverseBuild', {
+app.use(express.static('builds', {
   setHeaders: function(res, path) {
       if(path.endsWith(".gz")){
         res.set("Content-Encoding", "gzip")
@@ -22,17 +25,6 @@ app.use(express.static('builds/game-builds/MetaverseBuild', {
   }
 }))
 
-app.use(express.static('builds/minigame-chicken-run/WebGL Builds', {
-  setHeaders: function(res, path) {
-      if(path.endsWith(".gz")){
-        res.set("Content-Encoding", "gzip")
-      }
-      if(path.endsWith("wasm.gz")) {
-        res.set("Content-Type", "application/wasm")
-      }
-  }
-}))
-app.use('/', router);
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000...');
