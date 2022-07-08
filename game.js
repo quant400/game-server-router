82f140539466f28a00ec3cbbfa97f0e304af5715
Quant Cooker <quantcooker@gmail.com>
2022-07-08 22:44:51 +0300

    second new approach

--- game.js
+++ game.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-// const router = express.Router();
 const buildPath = __dirname + '/game-deployment-builds/'
 
+const chickenRouter = express.Router()
+const tutorialRouter = express.Router()
+const bearRouter = express.Router()
+
 app.get('/',function(req,res){
-  res.sendFile(path.join(buildPath + 'chicken-run/WebGL Builds/index.html'));
+  res.send('server is running...')
 });
 
-app.use(express.static('game-deployment-builds/chicken-run/WebGL Builds', {
+chickenRouter.get('/',function(req,res){
+  res.sendFile(path.join(buildPath + 'chicken-run/WebGL Builds/index.html'))
+});
+
+chickenRouter.use(express.static('game-deployment-builds/chicken-run/WebGL Builds', {
   setHeaders: function(res, path) {
       if(path.endsWith(".gz")){
         res.set("Content-Encoding", "gzip")
@@ -19,22 +26,40 @@ app.use(express.static('game-deployment-builds/chicken-run/WebGL Builds', {
   }
 }))
 
-// app.get('/metaverse',function(req,res){
-//   res.sendFile(path.join(buildPath + 'metaverse/WebGL Builds/index.html'));
-// });
+tutorialRouter.get('/',function(req,res){
+  res.sendFile(path.join(buildPath + 'tutorial/WebGL Builds/index.html'))
+});
 
-// app.use(express.static('game-deployment-builds/metaverse/WebGL Builds', {
-//   setHeaders: function(res, path) {
-//       if(path.endsWith(".gz")){
-//         res.set("Content-Encoding", "gzip")
-//       }
-//       if(path.endsWith("wasm.gz")) {
-//         res.set("Content-Type", "application/wasm")
-//       }
-//   }
-// }))
+tutorialRouter.use(express.static('game-deployment-builds/tutorial/WebGL Builds', {
+  setHeaders: function(res, path) {
+      if(path.endsWith(".gz")){
+        res.set("Content-Encoding", "gzip")
+      }
+      if(path.endsWith("wasm.gz")) {
+        res.set("Content-Type", "application/wasm")
+      }
+  }
+}))
 
-// app.use('/', router);
+bearRouter.get('/',function(req,res){
+  res.sendFile(path.join(buildPath + 'fight-the-bear/WebGL Builds/index.html'))
+});
+
+bearRouter.use(express.static('game-deployment-builds/fight-the-bear/WebGL Builds', {
+  setHeaders: function(res, path) {
+      if(path.endsWith(".gz")){
+        res.set("Content-Encoding", "gzip")
+      }
+      if(path.endsWith("wasm.gz")) {
+        res.set("Content-Type", "application/wasm")
+      }
+  }
+}))
+
+app.use('/tutorial', tutorialRouter)
+app.use('/fight-the-bear', bearRouter)
+app.use('/chicken-run', chickenRouter)
+
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000...');
